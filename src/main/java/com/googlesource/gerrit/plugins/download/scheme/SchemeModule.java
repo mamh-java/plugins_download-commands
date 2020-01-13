1f66152ff715874dc2477067fa4811a4f650a0fb
Mamh-Linux <bright.ma@blackshark.com>
2020-01-13 16:25:12 +0800

    remove scheme anonymous http git http repo
    Signed-off-by: Mamh-Linux <bright.ma@blackshark.com>

--- src/main/java/com/googlesource/gerrit/plugins/download/scheme/SchemeModule.java
+++ src/main/java/com/googlesource/gerrit/plugins/download/scheme/SchemeModule.java
@@ -22,22 +22,6 @@ public class SchemeModule extends AbstractModule {
   @Override
   protected void configure() {
     bind(DownloadScheme.class)
-      .annotatedWith(Exports.named("anonymous http"))
-      .to(AnonymousHttpScheme.class);
-
-    bind(DownloadScheme.class)
-      .annotatedWith(Exports.named("git"))
-      .to(GitScheme.class);
-
-    bind(DownloadScheme.class)
-      .annotatedWith(Exports.named("http"))
-      .to(HttpScheme.class);
-
-    bind(DownloadScheme.class)
-      .annotatedWith(Exports.named("repo"))
-      .to(RepoScheme.class);
-
-    bind(DownloadScheme.class)
       .annotatedWith(Exports.named("ssh"))
       .to(SshScheme.class);
   }
